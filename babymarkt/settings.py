e6aa2ddd0d6ecd8ad7839fc8631e4f716f331ba1
paulvhk <p.vanhecke86@gmail.com>
2020-06-27 15:12:15 +0200

    Update settings.py

--- babymarkt/settings.py
+++ babymarkt/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '++8)4!@4frw5=3f7!=mn%sa9m0^ptfg$pr+-#+q9p*i*9l%vpv'
+SECRET_KEY = 'zzz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
